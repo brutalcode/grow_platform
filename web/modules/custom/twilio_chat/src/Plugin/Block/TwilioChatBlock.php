5a6902fc86e117a965a2697f5a85238e3a427b71
Barys Nikulin <Barys_Nikulin@epam.com>
2018-01-24 18:16:49 +0300

    Add Twilio chat block

---
+++ web/modules/custom/twilio_chat/src/Plugin/Block/TwilioChatBlock.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\twilio_chat\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Config\ConfigFactory;
+
+/**
+ * Provides a 'TwilioChatBlock' block.
+ *
+ * @Block(
+ *  id = "twilio_chat_block",
+ *  admin_label = @Translation("Twilio chat block"),
+ * )
+ */
+class TwilioChatBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Config\ConfigFactory definition.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $configFactory;
+  /**
+   * Constructs a new TwilioChatBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    ConfigFactory $config_factory
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+//    $build['twilio_chat_block']['#markup'] = 'Implement TwilioChatBlock.';
+    $build['twilio_chat_block']['#theme'] = 'twilio_chat';
+
+    return $build;
+  }
+
+}
